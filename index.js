ccf7cc621cb145e41d5f0f6318b968b9ac7341f7
unknown <mm.farid@post.cz>
2022-09-22 08:40:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Misc Helpers
+function randomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+  function getKeyString(x, y) {
+    return `${x}x${y}`;
+  }
+
+
+  (function () {
+    firebase.auth().signInAnonymously().catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode, errorMessage);
+    })
+  } 
+  );
+
